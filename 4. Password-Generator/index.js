259b0ca8fa5842cda8a7c86aba8efed818f56fb4
Rohit Singh <jerryy1803@gmail.com>
2024-05-13 19:33:32 +0530

    password button issue solved

--- 4. Password-Generator/index.js
+++ 4. Password-Generator/index.js
@@ -12,7 +12,7 @@ function createPassword() {
   let password = "";
   password += upperCase[Math.floor(Math.random() * upperCase.length)];
   password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-  password += number[Math.floor(Math.random() * uppernumberCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
   password += symbol[Math.floor(Math.random() * symbol.length)];
 
   while (length > password.length) {
